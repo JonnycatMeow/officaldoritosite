5e8f6da043b8d2d112f1f1d07b3cb514e4ec5e81
Jonathan Gomez <jonnygomez156@gmail.com>
2024-01-22 00:30:56 -0800

    fixed

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,3 @@
 export const SUBTITLE = "i work on fnf stuff as a  source coder. I am also an ios porter for haxeflixel stuff 4 pm is 2:00 pm for you"
 export const TITLE = "FutureDorito"
 export const TAGLINE = "Apple developer/mobile developer"
-
-// Contacts
-export const contact = "ignore this"
-export const phone = "+33600000000"
-export const email = "contact@yoandev.co"
